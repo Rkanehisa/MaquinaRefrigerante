77a1e2fd695a295e925fb9f9422c76af6e47c6f8
Rkanehisa <rodrigokanehisa@gmail.com>
2016-11-23 15:06:58 -0300

    First Commit

---
+++ MaquinaRefrigerante/src/MaquinaRefrigerante.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author rkanehisa
+ *
+ */
+public class MaquinaRefrigerante {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+	}
+
+}
