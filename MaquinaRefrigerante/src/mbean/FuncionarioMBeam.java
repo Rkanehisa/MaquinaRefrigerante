cf30210c3a1ec61b66ccc49e4b029d2c510e70f5
alex <alex@mbpdealexandre.nca.local>
2016-12-09 13:13:56 -0300

    Adicionados metodos que filtram as latas, e validação de usuário.

--- MaquinaRefrigerante/src/mbean/FuncionarioMBeam.java
+++ MaquinaRefrigerante/src/mbean/FuncionarioMBeam.java
@@ -11,25 +11,23 @@ import dominio.Funcionario;
 
 public class FuncionarioMBeam {
 
-	private List<Funcionario> funcionarios = new LinkedList<Funcionario>();
 
-	public List<Funcionario> getListFuncionarios() throws SQLException {
-		if (!funcionarios.isEmpty()) {
-			return funcionarios;
-		}
+	public static boolean getListFuncionarios(String login, String senha) throws SQLException {
+		
 		MySQLAccess access = new MySQLAccess();
 		access.openConnection();
-		ResultSet rs = access.makeQuery("SELECT * FROM funcionario");
-		while(rs.next())
+		ResultSet rs = access.makeQuery("SELECT * FROM funcionario WHERE login='" + login + "' AND senha='" + senha + "';");
+		access.closeConnection();
+		if(rs.next())
 		{
-			int indice = rs.getInt("indice");
-			String login = rs.getString("login");
-			String senha = rs.getString("senha");
-			Funcionario funcionario = new Funcionario(indice, login, senha);
-			funcionarios.add(funcionario);
+			rs.close();
+			return true;
 		}
+		rs.close();
+		return false;
 
-		return funcionarios;
 	}
+	
+
 
 }
